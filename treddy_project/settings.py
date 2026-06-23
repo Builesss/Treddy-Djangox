88bf6819cccfd11bf76432ffd1a018292bcc2528
Sebastian Builes <sebastian.builes@treddy.com>
2026-06-23 00:49:55 -0500

    chore: migracion de base de datos de sqlite a supabase postgresql

--- treddy_project/settings.py
+++ treddy_project/settings.py
@@ -75,11 +75,14 @@ WSGI_APPLICATION = 'treddy_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://postgres:Cr9564821313@db.hyroajusvitpmlhivzmc.supabase.co:5432/postgres',
+        conn_max_age=600,
+        conn_health_checks=True,
+    )
 }
 
 
